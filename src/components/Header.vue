118e7f88d648a8ee9622da064466132ddae4e013
Adamsooo <adamsouedraogo222@gmail.com>
2025-06-30 16:28:01 +0200

    Tailwind n'est tjours pas bien configuré

---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class=bg-green-700 text-white p-4>
+    <h1 class=text-xl font-bold>Karibu UGE</h1>
+  </header>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class=text-center py-10>
+    <h1 class=text-3xl font-bold>Bienvenue sur le site de Karibu UGE</h1>
+    <p class=mt-4 text-gray-600>Découvre nos actions et notre équipe.</p>
+  </div>
+</template>
